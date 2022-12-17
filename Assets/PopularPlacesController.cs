3680e5ea1314d29d5f7f663654f2526a0aabee62
Marlon Subuyú <marlonsubuyu.2012@gmail.com>
2022-12-16 23:20:18 -0400

    Added Firebase

--- Assets/PopularPlacesController.cs
+++ Assets/PopularPlacesController.cs
@@ -1,29 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Firestore;
 using UnityEngine;
+using TMPro;
 
 public class PopularPlacesController : MonoBehaviour
 {
+    FirebaseFirestore Database;
+    public TMP_InputField TextInputField;
     void Start()
     {
-        // FirebaseFirestore.ini
+        Database = FirebaseFirestore.DefaultInstance;
     }
 
-    // async void Search(string QueryString)
-    // {
-    //     Query placesQuery = db.Collection("Lugares").WhereEqualTo("Nombre", QueryString);
-    //     QuerySnapshot placeQuerySnapshot = await placesQuery.GetSnapshotAsync();
-    //     foreach (DocumentSnapshot documentSnapshot in placeQuerySnapshot.Documents)
-    //     {
-    //         PopularPlaceModel popularPlaceModel = documentSnapshot.ConvertTo<PopularPlaceModel>();
-    //         Debug.Log("Document data for {0} document:", documentSnapshot.Id);
-    //         // Dictionary<string, object> city = documentSnapshot.ToDictionary();
-    //         // foreach (KeyValuePair<string, object> pair in city)
-    //         // {
-    //         //     Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
-    //         // }
-    //         // Console.WriteLine("");
-    //     }
-    //     // Program.cs
-    // }
+    public async void Search()
+    {
+        Query placesQuery = Database.Collection("Lugares").WhereEqualTo("Nombre", TextInputField.text);
+        QuerySnapshot placeQuerySnapshot = await placesQuery.GetSnapshotAsync();
+        foreach (DocumentSnapshot documentSnapshot in placeQuerySnapshot.Documents)
+        {
+            // PopularPlaceModel popularPlaceModel = documentSnapshot.ConvertTo<PopularPlaceModel>();
+            Debug.Log("Document data for {0} document:" + documentSnapshot.Id);
+            Dictionary<string, object> city = documentSnapshot.ToDictionary();
+            foreach (KeyValuePair<string, object> pair in city)
+            {
+                Debug.Log("{0}: {1}" + pair.Key + pair.Value);
+            }
+            // Console.WriteLine("");
+        }
+        // Program.cs
+    }
 }
